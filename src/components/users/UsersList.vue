83ead2556514e60944cf984b0ff4eb65cdeb6ccd
hossein <hr.movahed6@gmail.com>
2020-11-18 10:45:58 +0200

    some router featurs

---
+++ src/components/users/UsersList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="ConfirmInput">Confirm</button>
+    <ul>
+      <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+  components: {
+    UserItem
+  },
+  inject: ['users'],
+  methods: {
+    ConfirmInput() {
+      // changintg to teams
+      this.$router.push('/teams');
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,37 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'teams-member ',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        }
+      ]
+    },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+    { path: '/:catchAll(.*)', redirect: '/teams' }
+  ],
+  linkActiveClass: 'active'
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
